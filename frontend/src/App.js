7de1589cfda48e06785a281990d550c92d289c9f
Young Seok Seo <young7seo@gmail.com>
2020-03-22 16:24:21 -0700

    add logo

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,13 @@
 import React from "react";
+import styled from "styled-components";
+
+const S = {};
+
+S.Logo = styled.img`
+  position: fixed;
+  height: 200px; 
+`;
 
 export default () => (
-  <>
-    <h1>Welcome to React Parcel Micro App!</h1>
-    <p>Hard to get more minimal than this React app.</p>
-  </>
+  <S.Logo src="https://user-images.githubusercontent.com/46095809/77264774-51971880-6c58-11ea-854e-a432342d1075.png" />
 );
