2be2326d4b82954806eef67757b94ae0aa46f2eb
Young Seok Seo <young7seo@gmail.com>
2020-03-23 02:17:52 -0700

    edit last paragraph

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -149,7 +149,7 @@ const HomePage = () => {
                     Whether it be groceries, picking up prescriptions or any other errand, please do not hesitate to reach out; we will be more than happy to help! 
                     </S.TextParagraph>
                     <S.TextParagraphBold>
-                    Call or text the number below with a description of the errand you need. We will coordinate an estimated delivery time and your deliverer will provide receipts if applicable. If payment is needed (ex. groceries), this can be made via cash, cheque or e-transfer to the deliverer. 
+                    Call or text the number below with a description of the errand you need. We will coordinate an estimated delivery time with a deliverer and notify you as soon as we can. If payment is needed (ex. groceries), this can be made via cash, cheque or e-transfer to the deliverer. 
                     </S.TextParagraphBold>
                     
                 </S.Text>
